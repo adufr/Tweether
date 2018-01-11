1e254442bb1b779f5e84e9ae57667b185766c81b
Arthur 'Woosy <dufourarthur.perso@gmail.com>
2018-01-11 19:36:26 +0100

    Ajout fonction pour récupérer la date (formatée)

--- Utils.js
+++ Utils.js
@@ -20,3 +20,31 @@ var getHour = function () {
 
 // On rend les méthodes accessibles :
 exports.getHour = getHour;
+
+
+
+
+
+
+// ====================================================
+// ====================================================
+// == Date atuelle (au format jj/mm/aa)  ==============
+// ====================================================
+// ====================================================
+
+var getTodaysDate = function () {
+
+  // Permet d'afficher 01/01/18 au lieu de 1/1/18 :
+  function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    } return i;
+  }
+
+  date = new Date();
+  return addZero(date.getDate()) + "/" + addZero(date.getMonth()+1) + "/" + date.getFullYear();
+}
+
+
+// On rend les méthodes accessibles :
+exports.getTodaysDate = getTodaysDate;
